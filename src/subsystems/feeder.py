f46cf72eecb8eaec4927997de531bd0502100cdb
James Sokoll <jsokoll@ford.com>
2018-02-17 13:37:05 -0500

    Finished(?) feeder subsystem

--- src/subsystems/feeder.py
+++ src/subsystems/feeder.py
@@ -12,7 +12,8 @@ class Feeder(Subsystem):
     _enabled_key = "ENABLED"
     _channel_key = "CHANNEL"
     _inverted_key = "INVERTED"
-    _speed_scale_key = "MAX_SPEED"
+    _pickup_speed_scale_key = "PICKUP_SPEED"
+    _shoot_speed_scale_key = "SHOOT_SPEED"
 
     _left_motor_channel = None
     _right_motor_channel = None
@@ -26,20 +27,35 @@ class Feeder(Subsystem):
     _right_motor = None
     _switch = None
 
-    _has_cube = False
-    _speed_scale = 0.0
+    _pickup_speed_scale = 0.0
+    _shoot_speed_scale = 0.0
 
     def __init__(self, robot, name = None, configfile = '/home/lvuser/configs/subsystems.ini'):
         self._robot = robot
         self._config = configparser.ConfigParser()
         self._config.read(configfile)
-        self._init_components()
+        self.init_components()
         super().__init__(name = name)
 
-    # TODO: hasCube(), spinFeeder(), spinFeederLeft(), spinFeederRight()
+    def has_cube(self):
+        if self._switch:
+            return not self._switch.get()
+        else:
+            return False
 
-    def _init_components(self):
-        self._speed_scale = self._config.getfloat(self._general_section, self._speed_scale_key)
+    def spin_feeder(self, speed, left_should_spin, right_should_spin):
+        if speed > 0:
+            speed = speed * self._shoot_speed_scale
+        if speed < 0:
+            speed = speed * self._pickup_speed_scale
+        if self._left_motor and left_should_spin:
+            self._left_motor.set(speed)
+        if self._right_motor and right_should_spin:
+            self._right_motor.set(speed)
+
+    def init_components(self):
+        self._pickup_speed_scale = self._config.getfloat(self._general_section, self._pickup_speed_scale_key)
+        self._shoot_speed_scale = self._config.getfloat(self._general_section, self._shoot_speed_scale_key)
 
         if self._config.getboolean(Feeder._general_section, Feeder._enabled_key):
             self._left_motor_channel = self._config.getint(self._left_motor_section, self._channel_key)
