c643f863be2cf8ab8c3ac44ee35aab05dc9a8879
James Sokoll <jsokoll@ford.com>
2018-02-20 23:08:26 -0500

    Removed extra motor from feeder subsystem

--- src/subsystems/feeder.py
+++ src/subsystems/feeder.py
@@ -8,8 +8,6 @@ from wpilib.smartdashboard import SmartDashboard
 
 
 class Feeder(Subsystem):
-    _left_motor_section: str = "FeederMotorLeft"
-    _right_motor_section: str = "FeederMotorRight"
     _general_section: str = "FeederGeneral"
     _switch_section: str = "FeederSwitch"
 
@@ -19,17 +17,14 @@ class Feeder(Subsystem):
     _pickup_speed_scale_key: str = "PICKUP_SPEED"
     _shoot_speed_scale_key: str = "SHOOT_SPEED"
 
-    _left_motor_channel: int = None
-    _right_motor_channel: int = None
+    _motor_channel: int = None
     _switch_channel: int = None
 
-    _left_motor_inverted: bool = False
-    _right_motor_inverted: bool = False
+    _motor_inverted: bool = False
 
     _robot = None
     _config: ConfigParser = None
-    _left_motor: Talon = None
-    _right_motor: Talon = None
+    _motor: Talon = None
     _switch: DigitalInput = None
 
     _pickup_speed_scale: float = 0.0
@@ -53,20 +48,17 @@ class Feeder(Subsystem):
             return False
 
     def feed_cube(self, speed: float):
-        if self._left_motor and self._right_motor:
+        if self._motor:
             if speed > 0.0:
                 speed = speed * self._shoot_speed_scale
-                self._left_motor.set(speed)
-                self._right_motor.set(speed)
+                self._motor.set(speed)
             elif speed < 0.0 and not self.has_cube():
                 speed = speed * self._pickup_speed_scale
-                self._left_motor.set(speed)
-                self._right_motor.set(speed)
+                self._motor.set(speed)
             else:
                 speed = 0.0
-                self._left_motor.set(speed)
-                self._right_motor.set(speed)
-        self.update_smartdashboard()
+                self._motor.set(speed)
+        self._update_smartdashboard()
 
     def _update_smartdashboard(self):
         SmartDashboard.putBoolean("Cube Acquired", self.has_cube())
@@ -76,20 +68,13 @@ class Feeder(Subsystem):
         self._shoot_speed_scale = self._config.getfloat(self._general_section, self._shoot_speed_scale_key)
 
         if self._config.getboolean(Feeder._general_section, Feeder._enabled_key):
-            self._left_motor_channel = self._config.getint(self._left_motor_section, self._channel_key)
-            self._left_motor_inverted = self._config.getboolean(self._left_motor_section, self._inverted_key)
-            self._right_motor_channel = self._config.getint(self._right_motor_section, self._channel_key)
-            self._right_motor_inverted = self._config.getboolean(self._right_motor_section, self._inverted_key)
+            self._motor_channel = self._config.getint(self._general_section, self._channel_key)
+            self._motor_inverted = self._config.getboolean(self._general_section, self._inverted_key)
 
-        if self._left_motor_channel:
-            self._left_motor = Talon(self._left_motor_channel)
-            if self._left_motor_inverted:
-                self._left_motor.setInverted(self._left_motor_inverted)
-
-        if self._right_motor_channel:
-            self._right_motor = Talon(self._right_motor_channel)
-            if self._right_motor_inverted:
-                self._right_motor.setInverted(self._right_motor_inverted)
+        if self._motor_channel:
+            self._motor = Talon(self._motor_channel)
+            if self._motor_inverted:
+                self._motor.setInverted(self._motor_inverted)
 
         if self._config.getboolean(Feeder._switch_section, Feeder._enabled_key):
             self._switch_channel = self._config.getint(self._switch_section, self._channel_key)
