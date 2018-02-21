b8b290d1b3cd5e08e9ee33000e426af16b845496
James Sokoll <jsokoll@ford.com>
2018-02-20 21:59:35 -0500

    Used stopwatch class instead of rolling my own

--- src/commands/shoot_load.py
+++ src/commands/shoot_load.py
@@ -1,33 +1,39 @@
 from wpilib.command.command import Command
-import time
+from stopwatch import Stopwatch
+
 
 class ShootLoad(Command):
+    _stopwatch = None
+    _start_time = None
+    _duration = None
+    _speed = None
 
-    _time_stamp = 0
-
-    def __init__(self, robot, feedouttime: float, speed: float=0.0,, name=None, timeout=5):
+    def __init__(self, robot, duration, speed: float=0.0,, name=None, timeout=5):
         super().__init__(name, timeout)
-        self._robot = robot
-        self._feed_time = feedouttime
-        self._feeder_speed: float = speed
+        self.robot = robot
+        self._stopwatch = Stopwatch()
+        self._duration = duration
+        self._speed = speed
         self.requires(robot.feeder)
 
     def initialize(self):
         """Called before the Command is run for the first time."""
-        self._time_stamp = time.time()
+        self._stopwatch.start()
         return Command.initialize(self)
 
     def execute(self):
         """Called repeatedly when this Command is scheduled to run"""
-        self._robot.feeder.feed_cube(self._feeder_speed)
+        speed = self._speed
+        self._robot.feeder.feed_cube(speed)
         return Command.execute(self)
 
     def isFinished(self):
         """Returns true when the Command no longer needs to be run"""
-        return time.time() > self._time_stamp + self._feed_time
+        return self._stopwatch.elapsed_time_in_secs() >= self._duration
 
     def end(self):
         """Called once after isFinished returns true"""
+        self._stopwatch.stop()
         self._robot.feeder.feed_cube(0.0)
 
     def interrupted(self):
