8a16c934f8d11056c471f561cb04029dd0fb14e5
Aaron DeGrow <adeinpublic@gmail.com>
2018-02-24 15:48:15 -0500

    Alignment of config path parameters

--- src/subsystems/elevator.py
+++ src/subsystems/elevator.py
@@ -32,7 +32,7 @@ class Elevator(Subsystem):
     _encoder_top_bound = None
     _encoder_bottom_bound = None
 
-    def __init__(self, robot, name=None, configfile='/home/lvuser/configs/subsystems.ini'):
+    def __init__(self, robot, name=None, configfile='/home/lvuser/py/configs/subsystems.ini'):
         self._robot = robot
         self._subsystem_config = configfile
         self._init_components()
